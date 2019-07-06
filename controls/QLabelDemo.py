7ccc15e83fde465640079fc6bf1defda51eab093
sourcesakura <sourcesakura@sina.com>
2019-07-06 16:16:13 +0800

    label

--- controls/QLabelDemo.py
+++ controls/QLabelDemo.py
@@ -1,11 +1,11 @@
 import sys
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QMainWindow, QLabel, QHBoxLayout, QApplication, QVBoxLayout
+from PyQt5.QtGui import QPixmap, QPalette
+from PyQt5.QtWidgets import QMainWindow, QLabel, QHBoxLayout, QApplication, QVBoxLayout, QWidget
 
 
-class QLabelDemo(QMainWindow):
+class QLabelDemo(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
@@ -17,17 +17,24 @@ class QLabelDemo(QMainWindow):
         label4 = QLabel(self)
 
         label1.setText('<font color=red>text</font>')
+        # 背景色
+        label1.setAutoFillBackground(True)
+        palette = QPalette()
+        palette.setColor(QPalette.Window, Qt.blue)  # 设置背景色
+        label1.setPalette(palette)
         label1.setAlignment(Qt.AlignCenter)
 
         label2.setText('<a href="#">link</a>')
 
-        # label3.setAlignment(Qt.AlignCenter)
-        # label3.setToolTip('pic')
+        label3.setAlignment(Qt.AlignCenter)
+        label3.setToolTip('pic')
         # label3.setPixmap(QPixmap('./img/logo.png'))
 
         label4.setText('<a href="www.github.com">github</a>')
         label4.setAlignment(Qt.AlignRight)
         label4.setToolTip('link')
+        # true打开浏览器false触发事件
+        label4.setOpenExternalLinks(True)
 
         vLayout = QVBoxLayout()
 
@@ -42,7 +49,7 @@ class QLabelDemo(QMainWindow):
         self.setLayout(vLayout)
 
         self.setWindowTitle("Label show")
-        self.resize(400, 300)
+        # self.resize(400, 300)
 
     def hover(self):
         print('hover 2')
