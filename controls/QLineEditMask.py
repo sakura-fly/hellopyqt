f5f3ea71f8e22d091b9bd3749bdec09e3da1b023
sourcesakura <sourcesakura@sina.com>
2019-07-09 10:25:12 +0800

    文本改变事件

--- controls/QLineEditMask.py
+++ controls/QLineEditMask.py
@@ -21,12 +21,20 @@ class QLineEditMask(QWidget):
         dateLineEdit.setInputMask('0000-00-00')
         licenseLineEdit.setInputMask('>AAAAA-AAAAA-AAAAA-AAAAA-AAAAA;_')
 
+        ipLineEdit.textChanged.connect(self.printText)
+        macLineEdit.textChanged.connect(self.printText)
+        dateLineEdit.textChanged.connect(self.printText)
+        licenseLineEdit.textChanged.connect(self.printText)
+
         formLayout.addRow('数字掩码', ipLineEdit)
         formLayout.addRow('Mac掩码', macLineEdit)
         formLayout.addRow('日期掩码', dateLineEdit)
         formLayout.addRow('许可证掩码', licenseLineEdit)
 
         self.setLayout(formLayout)
+
+    def printText(self, text):
+        print(text)
 
 
 if __name__ == '__main__':
